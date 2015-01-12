ecfe6d2e35f5cffd54e4bf9c7fa105e8db43fdf6
U-ALLION\tharindu <tharindu@AT-DT-SLS-04.alliontechnologies.local>
2015-01-12 10:54:23 +0530

    initial commit structure is completed html page can access css and javascript files

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendfile('./public/html/index.html');
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('twitter analyser is  listening at http://%s:%s', host, port)
+
+})
