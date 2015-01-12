e9e9064adacd6b985a7d9180b0c8666759d86473
U-ALLION\tharindu <tharindu@AT-DT-SLS-04.alliontechnologies.local>
2015-01-12 11:46:30 +0530

    db connection is established all tweets can be read but db connection is not in a seperate file yet

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 var express = require('express')
 var app = express()
 
+//mongoosejs. this is to be inseted to a seperate file later
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://nishada:110330V@ds031271.mongolab.com:31271/twittersentiment');
+var db = mongoose.connection;
+//detect connection errors
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error:'));
+
+var tweetschema = new mongoose.Schema({
+	tweet_json : Object
+	, sentiment_json : Object
+	, saved_time : Object
+	}); 
+	var tweet = mongoose.model('tweets', tweetschema);
+
+
+db.once('open',function (callback){
+	console.log('connected');
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.sendfile('./public/html/index.html');
+})
+
+app.get('/all', function (req, res) {
+	//////////
+	
+	
+	tweet.find({}, function(err, data){
+		res.json(data);
+	})
+	///////////
+  
 })
 
 var server = app.listen(3000, function () {
